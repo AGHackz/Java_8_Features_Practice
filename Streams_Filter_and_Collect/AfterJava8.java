c37e905caf0b0d48e7de881bf48df78d81e4e699
AGHackz <ankit4579@gmail.com>
2017-10-04 00:10:26 +0530

    Comments Added for Java files

--- Streams_Filter_and_Collect/AfterJava8.java
+++ Streams_Filter_and_Collect/AfterJava8.java
@@ -8,16 +8,12 @@ import java.util.stream.Collectors;
 public class AfterJava8 {
     
     public static void main(String[] args) {
-        List<String> list = Arrays.asList("Kotlin", "Java", "Android", "Swift", "iOS");
-        List<String> filteredList = list.stream().filter(
-            item -> !item.equals("Java")
-        ).collect(
-            Collectors.toList()
-        );
+        List<String> list = Arrays.asList("Kotlin", "Java", "Android", "Swift", "iOS");                 // creates a list from these items
+        List<String> filteredList = list.stream()                                                       // converts list to stream
+                                        .filter(item -> !item.equals("Java"))                           // excluding "Java" from stream
+                                        .collect(Collectors.toList());                                  // collect items from stream to list 
 
-        filteredList.forEach(
-            item -> System.out.println(item)
-        );
+        filteredList.forEach(item -> System.out.println(item));                                         // print each item from the filteredList
     }
 
 }
--- Streams_Filter_and_Collect/BeforeJava8.java
+++ Streams_Filter_and_Collect/BeforeJava8.java
@@ -8,21 +8,21 @@ public class BeforeJava8 {
     
     public static void main(String[] args) {
         
-        List<String> lines = Arrays.asList("Java", "Android", "Swift", "iOS");
-        List<String> result = getFilterOutput(lines, "iOS");
+        List<String> lines = Arrays.asList("Java", "Android", "Objective C", "Swift", "iOS");          // creates a list from these items
+        List<String> result = getFilterOutput(lines, "Objective C");                                   // get filtered items
         for (String temp: result) {
-            System.out.println(temp);
+            System.out.println(temp);                                                                  // prints filtered items
         }
     }
 
     private static List<String> getFilterOutput(List<String> lines, String filter) {
         List<String> result = new ArrayList<>();
-        for (String line : lines) {
-            if (!filter.equals(line)) {
+        for (String line : lines) {                                                 // iterating items
+            if (!filter.equals(line)) {                                             // excluding item "Objective C" from list
                 result.add(line);
             }
         }
-        return result;
+        return result;                                                              // return filtered result
     }
 
 }
